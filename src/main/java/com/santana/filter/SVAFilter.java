8b917fd1c3ebca42e3062f8ae2acfb4a49fc951b
Thiago Santana <thiago.santana@gmail.com>
2017-07-03 13:52:14 -0300

    add a filter impl

--- src/main/java/com/santana/filter/SVAFilter.java
+++ src/main/java/com/santana/filter/SVAFilter.java
@@ -1,16 +1,16 @@
 package com.santana.filter;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.santana.vo.FailedActivationVO;
 
 public class SVAFilter {
 
 	public List<FailedActivationVO> filterByTimeoutError(List<FailedActivationVO> failedActivations) {
-		ArrayList lista = new ArrayList<>();
-		lista.add(null);
-		return lista;
+		Stream<FailedActivationVO> listWithOnlyTimeoutError = failedActivations.stream().filter(vo -> vo.getDS_LOG().contains("timed out"));
+		return listWithOnlyTimeoutError.collect(Collectors.toList());
 	}
 
 }
